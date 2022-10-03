00177cfebeaddab8c4b48ef1a07e8e9d5662f70a
gaetan <gaetan.ansotte@hotmail.com>
2022-10-03 18:54:27 +0200

    header with blank links UI

--- src/views/components/Header.vue
+++ src/views/components/Header.vue
@@ -1,7 +1,29 @@
 <template>
-    <header class="">
-        LAMAeazezaaez
-    </header>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-7 col-md-8 col-sm-10">
+                <!-- Top navigation -->
+                <div class="topnav">
+
+                    <!-- Centered link -->
+                    <div class="topnav-centered">
+                        <a href="#home" class="active">Home</a>
+                    </div>
+
+                    <!-- Left-aligned links (default) -->
+                    <a href="#news">News</a>
+                    <a href="#contact">Contact</a>
+
+                    <!-- Right-aligned links -->
+                    <div class="topnav-right">
+                        <a href="#search">Search</a>
+                        <a href="#about">About</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,10 +33,62 @@ export default {
 </script>
 
 <style scoped>
-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+/* Add a black background color to the top navigation */
+.topnav {
+    position: relative;
+    overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+    background-color: #e85122;
+    color: #28286e
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+    background-color: #e85122;
+    color: #28286e
+}
+
+/* Centered section inside the top navigation */
+.topnav-centered a {
+    float: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+/* Right-aligned section inside the top navigation */
+.topnav-right {
+    float: right;
+}
+
+/* Responsive navigation menu - display links on top of each other instead of next to each other (for mobile devices) */
+@media screen and (max-width: 600px) {
+
+    .topnav a,
+    .topnav-right {
+        float: none;
+        display: block;
+    }
+
+    .topnav-centered a {
+        position: relative;
+        top: 0;
+        left: 0;
+        transform: none;
+    }
 }
 </style>
