212701b9c22e149d78c19e2c9737ba0a56127642
gaetan <gaetan.ansotte@hotmail.com>
2022-10-11 18:10:57 +0200

    add L1L2 desc

--- src/views/L1L2.vue
+++ src/views/L1L2.vue
@@ -4,7 +4,10 @@
             <h2>L1 &rarr; L2 <i class="fas fa-archway"></i></h2>
         </div>
         <div class="card-body px-lg-5">
-            <h1>Work In Progress</h1>
+            <h1 class="text-center">Work In Progress</h1>
+            <h2>Based on some inputs: from_address, to_adress, selector, nonce, payload.<br />
+                It is possible to compute the L2 tx hash.<br />
+                This page will allow to do that.</h2>
         </div>
     </div>
 </template>
