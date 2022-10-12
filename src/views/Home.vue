2b96dbf8e0c0899e365708d418d6504f9935f549
gaetan <gaetan.ansotte@hotmail.com>
2022-10-12 16:55:04 +0200

    center home vertically

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pt-8 pt-md-12">
-    <h1 class="text-center">Stark-utils</h1>
+    <h1 class="pt-4 text-center">Stark-utils</h1>
     <h2 class="text-center">A Cairo toolbox</h2>
     <div class="text-center sm-12 pt-md-6">
       <router-link to="/converter">Open converter</router-link>
