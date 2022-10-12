0013c010da1f114d3e4c153c759224cdd25d1768
gaetan <gaetan.ansotte@hotmail.com>
2022-10-12 17:34:42 +0200

    toast when element copied

--- src/components/CopyComponent.vue
+++ src/components/CopyComponent.vue
@@ -6,11 +6,18 @@
         {{ valueToCopy }}
     </div>
     <br />
+
+    <div id="snackbar" v-show="showToast" class="show">Copied to clipboard</div>
 </template>
 
 <script>
 export default {
     name: 'CopyComponent',
+    data() {
+        return {
+            showToast: false
+        };
+    },
     props: {
         valueToCopy: String,
         isValid: {
@@ -24,9 +31,84 @@ export default {
     },
     methods: {
         async copy(text) {
+            this.showToast = true;
             await navigator.clipboard.writeText(text);
+            setTimeout(() => this.showToast = false, 3000);
         },
     },
 }
 </script>
 
+<style scoped>
+/* TODO add some global CSS vars */
+#snackbar {
+    visibility: hidden;
+    min-width: 200px;
+    margin-left: -125px;
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    border-radius: 10px;
+    padding: 12px;
+    position: fixed;
+    z-index: 1;
+    left: 50%;
+    bottom: 30px;
+}
+
+/* Show the snackbar when clicking on a button (class added with JavaScript) */
+#snackbar.show {
+    visibility: visible;
+    -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+    animation: fadein 0.5s, fadeout 0.5s 2.5s;
+}
+
+/* Animations to fade the snackbar in and out */
+@-webkit-keyframes fadein {
+    from {
+        bottom: 0;
+        opacity: 0;
+    }
+
+    to {
+        bottom: 30px;
+        opacity: 1;
+    }
+}
+
+@keyframes fadein {
+    from {
+        bottom: 0;
+        opacity: 0;
+    }
+
+    to {
+        bottom: 30px;
+        opacity: 1;
+    }
+}
+
+@-webkit-keyframes fadeout {
+    from {
+        bottom: 30px;
+        opacity: 1;
+    }
+
+    to {
+        bottom: 0;
+        opacity: 0;
+    }
+}
+
+@keyframes fadeout {
+    from {
+        bottom: 30px;
+        opacity: 1;
+    }
+
+    to {
+        bottom: 0;
+        opacity: 0;
+    }
+}
+</style>
