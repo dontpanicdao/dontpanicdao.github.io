eaf59b234d1601e49eac88c552eb5b09b450739c
gaetbout <gaetan.ansotte@hotmail.com>
2022-10-13 09:06:45 +0200

    Enable hash on router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory  } from "vue-router";
 
 import Home from "@/views/Home.vue";
 import Converter from "@/views/Converter.vue";
@@ -29,7 +29,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   linkActiveClass: "active",
   routes,
 });
