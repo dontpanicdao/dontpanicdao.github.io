c458a92df9fd74a07f20fcddfee18f91ec4214f3
gaetan <gaetan.ansotte@hotmail.com>
2022-10-17 18:15:10 +0200

    update router to createWebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@/views/Home.vue";
 import Converter from "@/views/Converter.vue";
@@ -29,7 +29,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   linkActiveClass: "active",
   routes,
 });
